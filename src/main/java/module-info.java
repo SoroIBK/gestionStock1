24769cafa0c3b685669a2bbaad836118395a7048
Soro <ibrahimsoro67@gmail.com>
2022-06-08 10:58:12 +0100

    initial commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.snm.gestionstock.gestionstock {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.snm.gestionstock.gestionstock to javafx.fxml;
